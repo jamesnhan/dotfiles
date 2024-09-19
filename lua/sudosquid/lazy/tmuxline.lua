afce67b1293ab9c6a1fbfc6ad381911013b2d026
James Nhan <james@jamesnhan.com>
2024-09-18 23:51:07 -0700

    feat: disable tmuxline for faster startup feat: disable showmode

--- lua/sudosquid/lazy/tmuxline.lua
+++ lua/sudosquid/lazy/tmuxline.lua
@@ -1,4 +1,10 @@
+-- Only uncomment this to initially install the plugin. Then, disable it to
+-- speed up Neovim startup time or else it will take forever to launch.
 return {
-    "edkolev/tmuxline.vim",
+    -- "edkolev/tmuxline.vim",
+    -- config = function()
+    --     vim.cmd.Tmuxline()
+    --     vim.cmd.TmuxlineSnapshot("$HOME/.tmux/tmuxline.vim!")
+    -- end,
 }
 
